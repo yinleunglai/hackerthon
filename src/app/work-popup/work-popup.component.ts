cf95e5da162fcaa0704df615e8d617a2b28b8f16
freedy <freedy.yang@outlook.com>
2019-12-08 12:01:33 +0800

    add popup

---
+++ src/app/work-popup/work-popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-popup',
+  templateUrl: './work-popup.component.html',
+  styleUrls: ['./work-popup.component.scss'],
+})
+export class WorkPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
